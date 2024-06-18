6e37f2a5289e005c0b62545c355d9faf3b037bdb
JavierArriagada <jarriagada4@gmail.com>
2024-06-18 08:59:26 -0400

    feat : add items and map safe_price and replace_coma

--- scr_pharma/items.py
+++ scr_pharma/items.py
@@ -4,13 +4,20 @@ from itemloaders.processors import TakeFirst, MapCompose
 def safe_price(value):
     try:
         # Convertir a cadena y limpiar, asegurando que se manejen casos donde el valor pueda estar vacío o mal formado
-        return str(value).replace('$', '').replace(',', '').strip() if value else '0'
+        return str(value).replace('$', '').replace(',', '').replace('.', '').strip() if value else '0'
     except Exception as e:
-        return '0'  # Retorna un valor predeterminado si algo sale mal
+        return '0'  
+
+def replace_comma(value):
+    try:
+        # Reemplazar comas por puntos
+        return str(value).replace(',', '.')
+    except Exception as e:
+        return value  
 
 class ScrPharmaItem(Item):
     name = Field(
-        input_processor=MapCompose(str.strip),
+        input_processor=MapCompose(replace_comma),
         output_processor=TakeFirst()
     )
     url = Field(
@@ -30,7 +37,10 @@ class ScrPharmaItem(Item):
     brand = Field(
         output_processor=TakeFirst()
     )
-    timestamp = Field() 
+    timestamp = Field(
+        input_processor=MapCompose(),
+        output_processor=TakeFirst()
+    ) 
     spider_name = Field(
         output_processor=TakeFirst()
     )
