afd3f1a6fd3555975e403971f1c727756c5d3b69
Javier <jarriagada4@gmail.com>
2024-06-22 20:47:15 -0400

    feat : Spider funcional, extrae datos primera pagina

--- scr_pharma/spiders/salcobrand.py
+++ scr_pharma/spiders/salcobrand.py
@@ -1,50 +1,91 @@
-import json
 import scrapy
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import NoSuchElementException
 from webdriver_manager.chrome import ChromeDriverManager
+import time
+from scrapy.loader import ItemLoader
+from datetime import datetime
+from ..items import ScrPharmaItem 
 
+
+import urllib.parse
 class SalcobrandSpider(scrapy.Spider):
     name = 'salcobrand'
-    allowed_domains = ['salcobrand.cl', 'gm3rp06hjg-dsn.algolia.net']
-    start_urls = ['https://salcobrand.cl/t/medicamentos']
-    api_base_url = 'https://gm3rp06hjg-dsn.algolia.net/1/indexes/*/queries'
-    api_params = '?x-algolia-agent=Algolia%20for%20JavaScript%20(4.14.3)%3B%20Browser%20(lite)&x-algolia-api-key=0259fe250b3be4b1326eb85e47aa7d81&x-algolia-application-id=GM3RP06HJG'
+    allowed_domains = ['salcobrand.cl']
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         chrome_options = Options()
-        #chrome_options.add_argument("--headless")
+        #chrome_options.add_argument("--headless")  # Uncomment for headless execution
         service = Service(ChromeDriverManager().install())
         self.driver = webdriver.Chrome(service=service, options=chrome_options)
+        self.categories = ['medicamentos']
 
     def start_requests(self):
-        self.driver.get(self.start_urls[0])
-        WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.TAG_NAME, 'body')))
-        cookies = {cookie['name']: cookie['value'] for cookie in self.driver.get_cookies()}
-        self.driver.quit()
-        yield scrapy.Request(f"{self.api_base_url}{self.api_params}&page=0", cookies=cookies, callback=self.parse_api)
+        yield scrapy.Request(url='https://salcobrand.cl', callback=self.parse, dont_filter=True)
 
-    def parse_api(self, response):
-        data = json.loads(response.text)
-        for hit in data['results'][0]['hits']:
-            yield {
-                'id': hit['id'],
-                'name': hit['name'],
-                'brand': hit['brand'],
-                'price': hit['normal_price'],
-                'image_url': hit['catalog_image_url']
-            }
-        # Check for more pages
-        if 'nbPages' in data['results'][0] and data['results'][0]['page'] < data['results'][0]['nbPages'] - 1:
-            next_page = data['results'][0]['page'] + 1
-            yield scrapy.Request(f"{self.api_base_url}{self.api_params}&page={next_page}", callback=self.parse_api)
+    def parse(self, response):
+        self.driver.get(response.url)
+        base_url = 'https://salcobrand.cl/t/'
+
+        for category in self.categories:
+            url = f"{base_url}{category}"
+            self.driver.get(url)
+            time.sleep(5)  # Wait for JavaScript to load contents
+            
+            try:
+                products = self.driver.find_elements(By.XPATH, "//div[contains(@class, 'product clickable')]")
+                
+                if not products:
+                    print(f"No products found for category {category}, breaking the loop.")
+                    break
+                
+                for product in products:
+                    loader = ItemLoader(item=ScrPharmaItem(), selector=product)
+                    brand, product_url, product_name, price, price_sale, price_benef, sku = self.extract_product_details(product)
+                    loader.add_value('brand', brand)
+                    loader.add_value('url', product_url)
+                    loader.add_value('name', product_name)
+                    loader.add_value('price', price)
+                    loader.add_value('price_sale', price_sale)
+                    loader.add_value('price_benef', price_benef)
+                    loader.add_value('code', sku)
+                    loader.add_value('category', category)
+                    loader.add_value('timestamp', datetime.now())
+                    loader.add_value('spider_name', self.name)
+                    yield loader.load_item()
+            
+            except NoSuchElementException:
+                print(f"No products found due to NoSuchElementException for category {category}, breaking the loop.")
+                break
+
+    def extract_product_details(self, product):
+        try:
+            brand = 'Default Brand'  # Adjust according to the site's structure if needed
+        except NoSuchElementException:
+            brand = 'No brand'
+        try:
+            product_url = product.find_element(By.XPATH, ".//a").get_attribute('href')
+            product_name = product.find_element(By.XPATH, ".//div[contains(@class, 'info')]//a//span[contains(@class, 'product-info')]").text
+            parsed_url = urllib.parse.urlparse(product_url)
+            params = urllib.parse.parse_qs(parsed_url.query)
+            sku = params.get('default_sku', [''])[0]
+            product_url = urllib.parse.urlunparse(parsed_url._replace(query=''))
+        except NoSuchElementException:
+            product_url = 'No URL'
+            product_name = 'No name'
+            sku = 'No SKU'
+        try:
+            price = product.find_element(By.XPATH, ".//div[contains(@class, 'info')]//a//div[contains(@class, 'product-prices')]//div[contains(@class, 'sale-price secondary-price')]//span").text
+        except NoSuchElementException:
+            price = '0'
+        price_benef = '0'  # Adjust this XPath to retrieve benefit price if available
+        price_sale = '0'  # Adjust this XPath to retrieve benefit price if available
+        
+        return brand, product_url, product_name, price, price_sale, price_benef, sku
 
     def closed(self, reason):
-        # Clean up Selenium driver
-        if self.driver.service.is_running:
-            self.driver.quit()
+        self.driver.quit()
