6bd090520de6a2b9f9d618a1af789785ad04351f
Javier <jarriagada4@gmail.com>
2024-06-23 11:23:48 -0400

    feat : cambio de logica

--- scr_pharma/spiders/profar.py
+++ scr_pharma/spiders/profar.py
@@ -1,121 +1,126 @@
 import scrapy
-import time
-import random
-from urllib.parse import urlparse, unquote, urljoin
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.keys import Keys
-from selenium.common.exceptions import TimeoutException, NoSuchElementException, ElementClickInterceptedException
+from selenium.common.exceptions import NoSuchElementException
 from webdriver_manager.chrome import ChromeDriverManager
-from fake_useragent import UserAgent
+import time
+from scrapy.loader import ItemLoader
+from datetime import datetime
+from ..items import ScrPharmaItem
 
 class ProfarSpider(scrapy.Spider):
     name = 'profar'
     allowed_domains = ['profar.cl']
-    #start_urls = ['https://www.profar.cl/medicamentos','https://www.profar.cl/dermocosmetica', 'https://www.profar.cl/belleza', 'https://www.profar.cl/cuidado-personal', 'https://www.profar.cl/salud-animal']
-    #start_urls = ['https://www.profar.cl/belleza', 'https://www.profar.cl/cuidado-personal', 'https://www.profar.cl/salud-animal']
-    start_urls = ['https://www.profar.cl/cuidado-personal']
-    '''
-    start_urls = [
-        'https://www.profar.cl/medicamentos/Antibioticos-y-Antivirales', 
-        'https://www.profar.cl/medicamentos/Antidiabeticos-y-Tiroides', 
-        'https://www.profar.cl/medicamentos/bienestar-sexual',
-        'https://www.profar.cl/medicamentos/Cardiovascular', 
-        'https://www.profar.cl/medicamentos/Coagulacion', 
-        'https://www.profar.cl/medicamentos/dermatologicos', 
-        'https://www.profar.cl/medicamentos/Dolor-y-Fiebre', 
-        'https://www.profar.cl/medicamentos/Enfermedades-especificas', 
-        'https://www.profar.cl/medicamentos/Gastrointestinal',
-        'https://www.profar.cl/medicamentos/Huesos-y-Articulaciones', 
-        'https://www.profar.cl/medicamentos/Oftalmicos-y-Oidos', 
-        'https://www.profar.cl/medicamentos/Respiratorio', 
-        'https://www.profar.cl/medicamentos/Salud-Femenina', 
-        'https://www.profar.cl/medicamentos/Salud-Masculina', 
-        'https://www.profar.cl/medicamentos/Vacunas', 
-        'https://www.profar.cl/medicamentos/Vitaminas-y-Minerales', 
-        'https://www.profar.cl/medicamentos/Otros'
-    ]
-    '''
+    start_urls = ['https://www.profar.cl']
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.ua = UserAgent()
-        options = Options()
-        options.add_experimental_option('excludeSwitches', ['enable-automation'])
-        options.add_experimental_option('useAutomationExtension', False)
-        options.add_argument(f'user-agent={self.ua.random}')
-        self.driver = webdriver.Chrome(options=options)
-        self.driver.execute_script("Object.defineProperty(navigator, 'webdriver', {get: () => undefined})")
+        chrome_options = Options()
+        # chrome_options.add_argument("--headless")  # Uncomment for headless execution
+        service = Service(ChromeDriverManager().install())
+        self.driver = webdriver.Chrome(service=service, options=chrome_options)
+        self.categories = [
+            'medicamentos',
+            'dermocosmetica',
+            'belleza',
+            'cuidado-personal',
+            'salud-animal'
+            ]
 
     def start_requests(self):
         for url in self.start_urls:
-            user_agent = self.ua.random  # Get a new user agent for each request
-            self.driver.execute_script(f"navigator.__defineGetter__('userAgent', function(){{return '{user_agent}';}});")
-            yield scrapy.Request(url=url, callback=self.parse, meta={'page_count': 0, 'user_agent': user_agent})
+            yield scrapy.Request(url=url, callback=self.parse, dont_filter=True)
 
     def parse(self, response):
         self.driver.get(response.url)
-        category = urlparse(response.url).path.lstrip('/').replace('https://www.profar.cl/', '')
-        page_count = response.meta['page_count']
-        click_count = 0
+        base_url = 'https://www.profar.cl/'
+        time.sleep(5)  # Esperar a que se cargue el JavaScript
+        for category in self.categories:
+            url = f"{base_url}{category}"
+            self.driver.get(url)
+            time.sleep(5)  # Esperar a que se cargue el JavaScript
+            
+            current_url = self.driver.current_url  # Almacenar la URL actual
+            while True:
+                self.scroll_to_pagination()  # Desplazarse al botón de paginación y hacer clic
+                time.sleep(2)  # Esperar a que carguen los productos
+                
+                new_url = self.driver.current_url  # Obtener la nueva URL después del clic
+                if current_url == new_url:
+                    print("URL has not changed after clicking 'Show more', stopping pagination.")
+                    break  # Si la URL no cambia, detener la paginación
+                else:
+                    current_url = new_url  # Actualizar la URL actual para la siguiente iteración
+                
+                # Desplazarse de nuevo al inicio de la página para asegurar que todos los elementos estén cargados
+                self.driver.execute_script("window.scrollTo(0, 0);")
+                time.sleep(2)  # Dar tiempo para que el scroll y la carga se completen
 
+            # Extracción de productos después de asegurar que todos los elementos estén visibles
+            products_after_click = self.driver.find_elements(By.XPATH, "//section[contains(@class, 'product-summary')]")
+            for product in products_after_click:
+                loader = ItemLoader(item=ScrPharmaItem(), selector=product)
+                brand, product_url, product_name, price, price_sale, price_benef, sku = self.extract_product_details(product)
+                loader.add_value('brand', brand)
+                loader.add_value('url', product_url)
+                loader.add_value('name', product_name)
+                loader.add_value('price', price)
+                loader.add_value('price_sale', price_sale)
+                loader.add_value('price_benef', price_benef)
+                loader.add_value('code', sku)
+                loader.add_value('category', category)
+                loader.add_value('timestamp', datetime.now())
+                loader.add_value('spider_name', self.name)
+                yield loader.load_item()
+
+    def scroll_to_pagination(self):
         try:
-            while click_count < 5:
-                self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight/3);")
-                time.sleep(random.uniform(0.5, 2))
+            # Desplazarse al final de la página primero para cargar todos los elementos
+            self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+            time.sleep(2)  # Esperar a que se carguen todos los productos
+            
+            # Luego, localizar el botón de cargar más resultados
+            load_more_button = self.driver.find_element(By.XPATH, "//button[contains(@class, 'vtex-button bw1 ba fw5 v-mid relative pa0 lh-solid br2 min-h-small t-action--small bg-action-primary b--action-primary c-on-action-primary hover-bg-action-primary hover-b--action-primary hover-c-on-action-primary pointer')]")
+            
+            # Hacer scroll hasta el botón
+            self.driver.execute_script("arguments[0].scrollIntoView(true);", load_more_button)
+            time.sleep(2)  # Pequeña espera para asegurar que el scroll se ha completado
 
-                try:
-                    load_more_button = WebDriverWait(self.driver, 10).until(
-                        EC.element_to_be_clickable((By.XPATH, "//button[contains(@class, 'pa0 lh-solid br2 min-h-small')]"))
-                    )
-                    load_more_button.click()  # Direct click without ActionChains
-                    click_count += 1
-                    time.sleep(random.uniform(0.5, 1.5))  # Wait a bit before the next operation
-                except ElementClickInterceptedException:
-                    self.driver.execute_script("arguments[0].click();", load_more_button)
-                except TimeoutException:
-                    break
-
-            sections = WebDriverWait(self.driver, 10).until(
-                EC.presence_of_all_elements_located((By.XPATH, "//section[contains(@class, 'product-summary')]"))
-            )
-            for section in sections:
-                product_name = self.get_text_or_default(section, ".//article//div[12]//span")
-                selling_price = self.get_text_or_default(section, ".//article//div[14]//span[contains(@class,'sellingPriceValue')]")
-                list_price = self.get_text_or_default(section, ".//article//div[14]//span[contains(@class,'listPriceValue')]")
-                product_url = self.get_attribute_or_default(section, ".//a", 'href')
-                product_prescription = self.get_text_or_default(section, ".//article//div[13]//span")
-
-                yield {
-                    'product_name': product_name,
-                    'selling_price': selling_price,
-                    'list_price': list_price,
-                    'product_url': product_url,
-                    'product_prescription': product_prescription,
-                    'category': category
-                }
-
-            if click_count == 5:
-                next_page = urljoin(response.url, f"?page={page_count + 5}")
-                yield scrapy.Request(url=next_page, callback=self.parse, meta={'page_count': page_count + 5})
-
-        except TimeoutException:
-            self.logger.error("Tiempo de espera excedido para la carga de la página")
-        finally:
-            self.driver.quit()
-
-    def get_text_or_default(self, element, xpath, default='No disponible'):
-        try:
-            return element.find_element(By.XPATH, xpath).text
+            # Hacer clic en el botón
+            self.driver.execute_script("arguments[0].click();", load_more_button)
         except NoSuchElementException:
-            return default
+            print("Load more button not found.")
 
-    def get_attribute_or_default(self, element, xpath, attribute, default='No disponible'):
+                
+    def extract_product_details(self, product):
         try:
-            return element.find_element(By.XPATH, xpath).get_attribute(attribute)
+            product_url = product.find_element(By.XPATH, ".//a").get_attribute('href')
         except NoSuchElementException:
-            return default
+            product_url = 'No URL'
+        try:
+            product_name = product.find_element(By.XPATH, ".//article//div[12]//span").text
+        except NoSuchElementException:
+            product_name = 'No name'
+        '''try:
+            brand = product.find_element(By.XPATH, ".//div[contains(@class, 'product-brand')]").text
+        except NoSuchElementException:
+            brand = 'No brand' 
+            '''
+        try:
+            price = product.find_element(By.XPATH, ".//article//div[14]//span[contains(@class,'sellingPriceValue')]").text
+        except NoSuchElementException:
+            price = 'No price'
+        try:
+            price_sale = product.find_element(By.XPATH, ".//article//div[14]//span[contains(@class,'listPriceValue')]").text
+        except NoSuchElementException:
+            price_sale = 'No price'
+        price_benef = '0'  # Adjust this XPath to retrieve benefit price if available
+        price_sale = '0'  # Adjust this XPath to retrieve sale price if available
+        sku = '0'  # Adjust this XPath to retrieve SKU if available
+        brand = 'Profar'
+        return brand, product_url, product_name, price, price_sale, price_benef, sku
+    
+    def closed(self, reason):
+        self.driver.quit()
