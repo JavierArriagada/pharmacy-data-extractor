691c9b8c6d82daf1bbfdb4aad0beddf273c700f5
Javier <jarriagada4@gmail.com>
2024-10-14 14:01:40 -0300

    db : new table definition

--- scr_pharma/db.sql
+++ scr_pharma/db.sql
@@ -1,15 +1,15 @@
 CREATE TABLE `scr_pharma` (
-  `id` INT UNSIGNED AUTO_INCREMENT,
-  `name` VARCHAR(255) NOT NULL,
-  `url` VARCHAR(255) NOT NULL,
-  `category` VARCHAR(255) NOT NULL,
-  `price` DECIMAL(10,2),
-  `price_sale` DECIMAL(10,2),
-  `brand` VARCHAR(255) NOT NULL,
-  `timestamp` DATETIME NOT NULL,
-  `spider_name` VARCHAR(255) NOT NULL,
-  `code` VARCHAR(255),
-  `price_benef` DECIMAL(10,2),
+  `id` int unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `url` varchar(255) NOT NULL,
+  `category` varchar(255) NOT NULL,
+  `price` decimal(10,2) DEFAULT NULL,
+  `price_sale` decimal(10,2) DEFAULT NULL,
+  `brand` varchar(255) NOT NULL,
+  `timestamp` datetime NOT NULL,
+  `spider_name` varchar(255) NOT NULL,
+  `code` varchar(255) DEFAULT NULL,
+  `price_benef` decimal(10,2) DEFAULT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `url_unique` (`url`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  KEY `url_index` (`url`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
